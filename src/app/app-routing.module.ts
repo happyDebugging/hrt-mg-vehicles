bc4242a689b14d7116d9f1dab95af4648c3945d4
happyDebugging <87609041+happyDebugging@users.noreply.github.com>
2025-06-11 23:12:23 +0200

    Added basic routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VehicleDetailsComponent } from './vehicle-details/vehicle-details.component';
+import { VehicleLinesComponent } from './vehicle-lines/vehicle-lines.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  //{ path: 'auth', component: AuthComponent, pathMatch: 'full', canActivate: [LoggedInGuard]}, 
+  //{ path: 'reset-password/session/:session-id', pathMatch: 'full', component: ResetPasswordComponent},
+
+  { path: '', pathMatch: 'full', component: VehicleLinesComponent }, //, canActivate: [AuthGuard] // redirect to `app-component`  { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+  { path: '**', pathMatch: 'full', redirectTo: ''},  //, canActivate: [AuthGuard] // Wildcard route for a 404 page
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
