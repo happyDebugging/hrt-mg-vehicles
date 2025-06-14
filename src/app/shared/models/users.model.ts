e20adea88244e7e8fc98310c32f1f89ff328dc50
happyDebugging <87609041+happyDebugging@users.noreply.github.com>
2025-06-15 01:27:01 +0200

    Added app users data model.

---
+++ src/app/shared/models/users.model.ts
@@ -0,0 +1,9 @@
+export class Users {
+    Id!: any;
+    UserId!: any;
+    FirstName!: string;
+    LastName!: string;
+    Email!: string;
+    Permissions!: string;
+    HasChangedPassword!: boolean;
+}
