cdc3fb9724e6268d8e2e668ea7683c7f99deed01
happyDebugging <87609041+happyDebugging@users.noreply.github.com>
2025-06-15 01:13:54 +0200

    Customize vehicle details component.

--- src/app/vehicle-details/vehicle-details.component.ts
+++ src/app/vehicle-details/vehicle-details.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class VehicleDetailsComponent {
 
+  vehicleToPreview = '';
+  vehicleName = '';
+
+  kilometersSum = 0;
+  kteoExpiryDate = '';
+  insuranceExpiryDate = '';
+  lastServiceDate = '';
+  carTiresReplacementDate = '';
+  carExhaustExpiryDate = '';
+  fuelAdditionCost = 0;
+  fuelAdditionDate = '';
+
+  ngOnInit() {
+    this.vehicleToPreview = JSON.parse(JSON.stringify(sessionStorage.getItem('vehicleToPreview')));
+    this.vehicleName = this.vehicleToPreview.replaceAll('-',' ');
+  }
+
 }
