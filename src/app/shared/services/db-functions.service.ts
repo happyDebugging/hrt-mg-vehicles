ed11be578ef7129c636a5049c23372023bd92a09
happyDebugging <87609041+happyDebugging@users.noreply.github.com>
2025-06-21 23:01:52 +0200

    Updated DbFunctionService to save/fetch Vehicle Details and Registration Certificate to db.

---
+++ src/app/shared/services/db-functions.service.ts
@@ -0,0 +1,191 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { Users } from '../models/users.model';
+import { HistoryLines } from '../models/history-lines.model';
+import { createClient, Session, SupabaseClient, User } from '@supabase/supabase-js'
+import { VehicleDetails } from '../models/vehicle-details.model';
+
+@Injectable()
+export class DbFunctionService {
+
+    // Initialize Supabase
+    private supabase: SupabaseClient
+
+    constructor(private http: HttpClient) {
+        this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey);
+    }
+
+    async getVehicleDetailsFromDb() {
+        
+        const data = await this.supabase.from('vehicleDetails').select('*').order('Id', { ascending: false }).limit(1); 
+                    //.eq('StorageCategory', 'Τμήμα Πρώτων Βοηθειών');
+
+        return data["data"];
+    }
+
+    async postVehicleDetailsToDb(vehicleDetails: VehicleDetails) {
+        
+        const data = await this.supabase.from('materialLines')
+            .insert({
+                VehicleName: vehicleDetails.VehicleName,
+                VehicleType: vehicleDetails.VehicleType,
+
+                KilometersSum: vehicleDetails.KilometersSum,
+                KteoExpiryDate: vehicleDetails.KteoExpiryDate,
+                InsuranceExpiryDate: vehicleDetails.InsuranceExpiryDate,
+                LastServiceDate: vehicleDetails.LastServiceDate,
+                CarTiresReplacementDate: vehicleDetails.CarTiresReplacementDate,
+                CarExhaustExpiryDate: vehicleDetails.CarExhaustExpiryDate,
+                FuelAdditionCost: vehicleDetails.FuelAdditionCost,
+                FuelAdditionDate: vehicleDetails.FuelAdditionDate,
+
+                LastUpdatedAt: vehicleDetails.LastUpdatedAt,
+                LastUpdatedBy: vehicleDetails.LastUpdatedBy,
+                
+                Photo: vehicleDetails.Photo,
+                RegistrationCertificate: vehicleDetails.RegistrationCertificate,
+            }).select();
+
+        return data;
+    }
+
+
+    async getRegistrationCertificateFromDb(registrationCertificate: string) {
+        
+        const data = await this.supabase.storage.from('hrt-mg-vehicles-storage').getPublicUrl(registrationCertificate);
+
+        return data["data"];
+    }
+
+    async postRegistrationCertificateToDb(registrationCertificate: File) {
+        
+        //console.log(registrationCertificate)
+        // const data = await this.supabase.storage.from('hrt-mg-vehicles-storage')
+        //     .upload(materialImage + '.jpg', registrationCertificate[0], { cacheControl: '3600', upsert: false, contentType: 'image/jpeg' });
+        const data = await this.supabase.storage.from('hrt-mg-vehicles-storage')
+                .upload(`pdfs/${registrationCertificate.name}`, registrationCertificate);
+
+        //if (error) throw error;
+        console.log('Upload successful:', data);
+
+        return data["data"];
+    }
+
+    // async deleteMaterialPhotoFromDb(materialImage: string) {
+    //     let options: any = {
+    //         headers: { "Access-Control-Allow-Origin": "*" },
+    //         observe: 'response'
+    //     }
+    //     console.log(materialImage + '.jpg')
+    //     const { error } = await this.supabase.storage.from('hrt-mg-warehouse-photo-storage')
+    //         .remove([materialImage + '.jpg']);
+
+    //     return { error };
+    // }
+
+
+    // async addNewUserToDb(userΙd: string, newUserFirstName: string, newUserLastName: string, newUserEmail: string, newUserPermissions: string) {
+
+    //     const data = await this.supabase.from('users')
+    //         .insert({
+    //             UserId: userΙd,
+    //             FirstName: newUserFirstName,
+    //             LastName: newUserLastName,
+    //             Email: newUserEmail,
+    //             Permissions: newUserPermissions,
+    //             HasChangedPassword: false
+    //         }).select();
+
+    //     return data;
+    // }
+
+    // async updateUserToDb(userΙd: string, userFirstName: string, userLastName: string, userEmail: string, userPermissions: string) {
+
+    //     const data = await this.supabase.from('users')
+    //         .update({
+    //             UserId: userΙd,
+    //             FirstName: userFirstName,
+    //             LastName: userLastName,
+    //             Email: userEmail,
+    //             Permissions: userPermissions,
+    //             HasChangedPassword: false
+    //         }).eq('UserId', userΙd);
+
+    //     return data;
+    // }
+
+    // async deleteUserFromDb(userΙd: string) {
+
+    //     const { data, error } = await this.supabase.from('users')
+    //         .delete().eq('UserId', userΙd);
+
+    //     return { data, error };
+    // }
+
+    // async getUserDetailsFromDb(userEmail: string) {
+    //     let options: any = {
+    //         headers: { "Access-Control-Allow-Origin": "*" },
+    //         observe: 'response'
+    //     }
+    //     //return this.http.get<Users>(environment.databaseURL + environment.usersTable + '.json');
+    //     const data = await this.supabase.from('users').select('*').eq('Email', userEmail);
+
+    //     return data["data"];
+    // }
+
+    // async updateUserDetailsToDb(user: Users) {
+    //     let options: any = {
+    //         headers: { "Access-Control-Allow-Origin": "*" },
+    //         observe: 'response'
+    //     }
+    //     //return this.http.put(environment.databaseURL + environment.usersTable + '/' + user.UserId + '.json', user, options);
+    //     const data = await this.supabase.from('users').update({
+    //         UserId: user.UserId,
+    //         FirstName: user.FirstName,
+    //         LastName: user.LastName,
+    //         Email: user.Email,
+    //         Permissions: user.Permissions,
+    //         HasChangedPassword: user.HasChangedPassword
+    //     }).eq('Email', user.Email);
+
+    //     return data;
+    // }
+
+
+    // async geHistoryLinesFromDb(pageNumber: number, itemsPerPage: number) {
+    //     let options: any = {
+    //         headers: { "Access-Control-Allow-Origin": "*" },
+    //         observe: 'response'
+    //     }
+    //     //return this.http.get<HistoryLines>(environment.databaseURL + environment.historyLinesTable + '.json');
+    //     const data = await this.supabase.from('history').select('*', { count: 'exact' })
+    //         .order('Id', { ascending: false })
+    //         .range((pageNumber - 1) * itemsPerPage,
+    //             pageNumber * itemsPerPage - 1);
+
+    //     return data["data"];
+    // }
+
+    // async postHistoryLinesToDb(historyLine: HistoryLines) {
+    //     let options: any = {
+    //         headers: { "Access-Control-Allow-Origin": "*" },
+    //         observe: 'response'
+    //     }
+    //     //return this.http.post(environment.databaseURL + environment.historyLinesTable + '.json', historyLine, options);
+    //     const data = await this.supabase.from('history')
+    //         .insert({
+    //             Date: historyLine.Date,
+    //             ActionType: historyLine.ActionType,
+    //             MaterialName: historyLine.MaterialName,
+    //             SerialNumber: historyLine.SerialNumber,
+    //             Responsible: historyLine.Responsible
+    //         }).select();
+
+    //     return data;
+    // }
+
+}
+
+
+
