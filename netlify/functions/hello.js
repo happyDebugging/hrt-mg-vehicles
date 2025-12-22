c4dc65ff28b5773762c0e04ac60ba36124101a69
happyDebugging <87609041+happyDebugging@users.noreply.github.com>
2025-12-22 21:47:59 +0200

    Add Netlify Actions for API calls.

---
+++ netlify/functions/hello.js
@@ -0,0 +1,12 @@
+exports.handler = async (event) => {
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    },
+    body: JSON.stringify({
+      message: "Hello from Netlify Functions!"
+    })
+  };
+};
