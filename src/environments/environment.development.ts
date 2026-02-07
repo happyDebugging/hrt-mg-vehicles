14aa93aee102487d51af59087548d8f5898d3a8e
happyDebugging <87609041+happyDebugging@users.noreply.github.com>
2026-02-08 00:09:20 +0200

    Remove secrets from project.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -7,7 +7,4 @@ export const environment = {
     usersTable: '/users',
     historyLinesTable: '/history',
 
-    supabaseUrl: 'https://ccuxygolitirdpzhfkvr.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjdXh5Z29saXRpcmRwemhma3ZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzMDkwNzgsImV4cCI6MjA1OTg4NTA3OH0.lZHI2fl-blEv8B3xZMs77EOhlrgFK-AjI5ey-3W99_U',
-    
 };
