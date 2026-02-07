5a54b004f1ee8b626c3fca12965b55871bc63570
happyDebugging <87609041+happyDebugging@users.noreply.github.com>
2026-02-08 00:26:42 +0200

    Customize auth component & routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { VehicleDetailsComponent } from './vehicle-details/vehicle-details.component';
 import { VehicleLinesComponent } from './vehicle-lines/vehicle-lines.component';
+import { AuthComponent } from './auth/auth.component';
 
 const routes: Routes = [
 
-  //{ path: 'auth', component: AuthComponent, pathMatch: 'full', canActivate: [LoggedInGuard]}, 
+  { path: 'auth', component: AuthComponent, pathMatch: 'full'}, //, canActivate: [LoggedInGuard]
   //{ path: 'reset-password/session/:session-id', pathMatch: 'full', component: ResetPasswordComponent},
 
   {path: 'vehicle/:vehicle-id', component: VehicleDetailsComponent}, //, canActivate: [AuthGuard]
 
-  { path: '', pathMatch: 'full', component: VehicleLinesComponent }, //, canActivate: [AuthGuard] // redirect to `app-component`  { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+  { path: '', pathMatch: 'full', component: AuthComponent }, //, canActivate: [AuthGuard] // redirect to `app-component`  { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
   { path: '**', pathMatch: 'full', redirectTo: ''},  //, canActivate: [AuthGuard] // Wildcard route for a 404 page
 
 ];
