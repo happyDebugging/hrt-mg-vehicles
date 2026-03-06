34d1c36e7e11ba9d239748437d17eefefe480aac
HappyDebugging <squirrelinthewoods@protonmail.com>
2026-03-06 20:30:20 +0200

    Fix previously-used Netlify server.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
