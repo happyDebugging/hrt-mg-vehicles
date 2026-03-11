832c847feb02953257d302cfb041f96a97089d66
HappyDebugging <squirrelinthewoods@protonmail.com>
2026-03-11 23:54:16 +0200

    Modify logbook component
    Enable the pdf-download of the vehicles/boats history logs.

--- src/app/logbook/logbook.component.ts
+++ src/app/logbook/logbook.component.ts
@@ -1,6 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { DbFunctionService } from '../shared/services/db-functions.service';
 import { VehicleDetails } from '../shared/models/vehicle-details.model';
+import pdfMake from 'pdfmake/build/pdfmake';
+import vfsFonts from 'pdfmake/build/vfs_fonts';
+(pdfMake as any).vfs = vfsFonts;
+(pdfMake as any).fonts = {
+  Roboto: {
+    normal: 'Roboto-Regular.ttf',
+    bold: 'Roboto-Medium.ttf',
+    italics: 'Roboto-Italic.ttf',
+    bolditalics: 'Roboto-MediumItalic.ttf'
+  }
+};
+import { formatDate } from '@angular/common';
+import type { TDocumentDefinition } from 'pdfmake/interfaces';
 
 @Component({
   selector: 'app-logbook',
@@ -168,8 +181,102 @@ export class LogbookComponent implements OnInit {
     return Math.ceil(this.getBoatsOnly().length / this.boatPageSize);
   }
 
-  downloadHistoryLog(vehicleType: string) {
+  exportHistoryLog(vehicleType: string) {
+    const docDefinition: any = {
+      pageSize: 'A4',
+      pageOrientation: 'landscape',
+      header: function (currentPage: number, pageCount: number) {
+        return [
+          { text: 'Ιστορικό Οχημάτων ΕΟΔ Μαγνησίας', alignment: 'right', fontSize: 12, color: 'grey', margin: [480, 10, 40, 0] } // Right side text
+        ];
+      },
+      footer: function (currentPage: number, pageCount: number) {
+        return {
+          text: `Σελίδα ${currentPage}/${pageCount}`,
+          alignment: 'center',
+          fontSize: 10,
+          color: 'grey',
+          margin: [0, 10, 0, 0]
+        };
+      }
+    };
 
+    if (vehicleType === 'vehicle') {
+      docDefinition.content = [
+        { text: 'Ιστορικό Οχημάτων', style: 'header', fontSize: 15, color: '#154c79', bold: true },
+        { text: ' ', style: 'header' },
+        { text: ' ', style: 'header' },
+        { text: ' ', style: 'header' },
+        {
+          layout: 'lightHorizontalLines', // optional
+          fontSize: 8,
+          table: {
+            headerRows: 1,
+            widths: [30, 30, 30, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 30],
+            body: [
+              ['Ενημερωση', 'Οχημα', 'Οδηγος', 'Συν. Χλμ.', 'Αρχικά Χλμ.', 'Τελικά Χλμ.', 'Ημ/νια Χρησης', 'ΚΤΕΟ', 'Ασφαλεια', 'Service', 'Χλμ. Τελ. Service', 'Επομενο Service', 'Χλμ. Επομενου Service', 'Αλλαγη Ελαστικων', 'Καρτα Καυσαεριων', 'Καυσιμα(€)', 'Καυσιμα(lt)', 'Προσθηκη Καυσιμου', 'Παρατηρησεις'],
+              ...this.vehicleDetails.map(item => [
+                item.LastUpdatedAt,
+                item.VehicleName,
+                item.LastUpdatedByName,
+                item.TotalKm,
+                item.StartingKmOfShift,
+                item.FinalKmOfShift,
+                item.LastDrivenAt,
+                item.KteoExpiryDate,
+                item.InsuranceExpiryDate,
+                item.LastServiceDate,
+                item.LastServiceKilometers,
+                item.NextServiceDate,
+                item.NextServiceKilometers,
+                item.CarTiresReplacementDate,
+                item.CarExhaustExpiryDate,
+                item.FuelAdditionCost,
+                item.FuelAdditionLiters,
+                item.FuelAdditionDate,
+                item.Notes
+              ])
+            ]
+          }
+        }];
+    } else if (vehicleType === 'boat') {
+      docDefinition.content = [
+        { text: 'Ιστορικό Σκαφών', style: 'header', fontSize: 15, color: '#154c79', bold: true },
+        { text: ' ', style: 'header' },
+        { text: ' ', style: 'header' },
+        { text: ' ', style: 'header' },
+        {
+          layout: 'lightHorizontalLines', // optional
+          fontSize: 8,
+          table: {
+            headerRows: 1,
+            widths: [40, 40, 40, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 50],
+            fontSize: 8,
+            body: [
+              ['Ενημερωση', 'Σκαφος', 'Οδηγος', 'Ημ/νια Χρησης', 'Ωρες Μηχ. A', 'Ωρες Μηχ. B', 'Επιπεδο Καυσιμου', 'Επιπεδο Λαδιου', 'Συνολ. Ωρες Λειτουργιας', 'Προσθηκη Καυσιμου(lt)', 'Προσθηκη Καυσιμου', 'Προσθηκη Λαδιου(lt)', 'Προσθηκη Λαδιου', 'Παρατηρησεις'],
+              ...this.boatDetails.map(item => [
+                item.LastUpdatedAt,
+                item.VehicleName,
+                item.LastUpdatedByName,
+                item.DateOfBoatUse,
+                item.EngineAHours,
+                item.EngineBHours,
+                item.BoatFuelLevel,
+                item.BoatOilLevel,
+                item.BoatTotalOperatingHours,
+                item.BoatGasAdditionLiters,
+                item.BoatGasAdditionDate,
+                item.BoatOilAdditionLiters,
+                item.BoatOilAdditionDate,
+                item.Notes
+              ])
+            ]
+          }
+        }];
+
+    }
+
+    pdfMake.createPdf(docDefinition).download('vehicle_log' + '_' + formatDate(Date.now(), 'ddMMyy_hhmmss', 'en_US') + '.pdf');
   }
 
 }
